cd848a75e393349b194c4e736a44b62f3ee3ef21
Pawlox4E <72540552+Pawlox4E@users.noreply.github.com>
2021-11-25 13:35:46 +0100

    Add files via upload

--- sklep.sql
+++ sklep.sql
@@ -5,25 +5,12 @@ USE sklep;
 DROP TABLE IF EXISTS `pracownik`;
 CREATE TABLE `pracownik` (
     `id_pracownik` INT NOT NULL AUTO_INCREMENT,
-    `imie` VARCHAR(48) NOT NULL,
-    `nazwisko` VARCHAR(48)  NOT NULL,
-    `miejscowosc` VARCHAR(48) NOT NULL,
-    `ulica` VARCHAR(48) NOT NULL,
-    `numer_domu` VARCHAR(6) NOT NULL,
-    `nr_telefonu` VARCHAR(12) NOT NULL,
-    `id_stanowisko` INT NOT NULL,
+    `id_uzytkownik` INT NOT NULL,
+    `stanowisko` varchar(32) NOT NULL,
     `Data_zatrudnienia` DATE NOT NULL,
     `Data_zwolnienia` DATE DEFAULT NULL,
     PRIMARY KEY (`id_pracownik`)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
-
-DROP TABLE IF EXISTS `stanowiska`;
-CREATE TABLE `stanowiska` (
-  `id_stanowisko` INT NOT NULL AUTO_INCREMENT,
-  `nazwa` VARCHAR(48) NOT NULL,
-  `placa` DECIMAL(9,2) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_stanowisko`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `produkt`;
 CREATE TABLE `produkt` (
@@ -31,72 +18,57 @@ CREATE TABLE `produkt` (
     `nazwa` VARCHAR(48) DEFAULT NULL,
     `kategoria` VARCHAR(48) NOT NULL,
     `typ` VARCHAR(48) NOT NULL,
-    `id_zdjecia` INT NOT NULL,
+    `wariant` VARCHAR(48) NOT NULL,
+    `ilosc` int default null,
+    `cena_kupna` decimal default null,
+    `cena_sprzedaz` decimal default null,
     PRIMARY KEY (`id_produkt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `zdjecia`;
-CREATE TABLE `zdjecia` (
-	`id_zdjecia` INT NOT NULL AUTO_INCREMENT,
-    `dodatkowezdj` VARCHAR(48) DEFAULT NULL,
-    PRIMARY KEY (`id_zdjecia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `zamowienie`;
 CREATE TABLE `zamowienie` (
 	`id_zamowienie` INT NOT NULL AUTO_INCREMENT,
     `data_zamowienia` DATE DEFAULT NULL,
-    `id_klient` INT NOT NULL,
+    `id_uzytkownik` INT NOT NULL,
     `id_produkt` INT NOT NULL,
     `id_pracownik` INT NOT NULL,
+    `wartosc` decimal not null,
+    `status` varchar(64) not null,
     PRIMARY KEY (`id_zamowienie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `klient`;
-CREATE TABLE `klient` (
-	`id_klient` INT NOT NULL AUTO_INCREMENT,
-    `imie` VARCHAR(48) DEFAULT NULL,
-    `nazwisko` VARCHAR(48) NOT NULL,
-    `miejscowosc` VARCHAR(48) NOT NULL,
-    `ulica` VARCHAR(48) NOT NULL,
-    `numer_domu` VARCHAR(6) NOT NULL,
-    `nr_telefonu` VARCHAR(12) NOT NULL,
-    `email` VARCHAR(48) NOT NULL,
-    PRIMARY KEY (`id_klient`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `uzytkownik`;
 CREATE TABLE `uzytkownik` (
+	`id_uzytkownik` INT NOT NULL AUTO_INCREMENT,
 	`login` VARCHAR(48) NOT NULL,
-    `id_klient` INT DEFAULT NULL,
-    `haslo` VARCHAR(48) NOT NULL,
-    `id_koszyk` INT NOT NULL,
-    PRIMARY KEY (`login`)
+    `haslo` VARCHAR(300) NOT NULL,
+    `miejscowosc` VARCHAR(48) NOT NULL,
+    `ulica` VARCHAR(48) NOT NULL,
+    `numer_domu` VARCHAR(6) NOT NULL,
+    `nr_telefonu` VARCHAR(12) NOT NULL,
+    `czy_admin` boolean,
+    `email` varchar(128),
+    `id_koszyk` INT DEFAULT NULL,
+    `id_pracownik` INT DEFAULT NULL,
+    PRIMARY KEY (`id_uzytkownik`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `koszyk`;
 CREATE TABLE `koszyk` (
 	`id_koszyk` INT NOT NULL AUTO_INCREMENT,
     `id_zamowienie` INT DEFAULT NULL,
+    `id_uzytkownik` INT DEFAULT NULL,
     PRIMARY KEY (`id_koszyk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE pracownik ADD UNIQUE INDEX (id_pracownik);
-ALTER TABLE stanowiska ADD UNIQUE INDEX (id_stanowisko);
-ALTER TABLE produkt ADD UNIQUE INDEX (id_produkt);
-ALTER TABLE zdjecia ADD UNIQUE INDEX (id_zdjecia);
-ALTER TABLE zamowienie ADD UNIQUE INDEX (id_zamowienie);
-ALTER TABLE klient ADD UNIQUE INDEX (id_klient);
-ALTER TABLE uzytkownik ADD UNIQUE INDEX (login);
-ALTER TABLE koszyk ADD UNIQUE INDEX (id_koszyk);
+ALTER TABLE pracownik ADD UNIQUE INDEX (`id_pracownik`);
+ALTER TABLE produkt ADD UNIQUE INDEX (`id_produkt`);
+ALTER TABLE zamowienie ADD UNIQUE INDEX (`id_zamowienie`);
+ALTER TABLE uzytkownik ADD UNIQUE INDEX (`id_pracownik`);
+ALTER TABLE koszyk ADD UNIQUE INDEX (`id_koszyk`);
 
-
-ALTER TABLE pracownik ADD CONSTRAINT FOREIGN KEY (`id_stanowisko`) REFERENCES `stanowiska`(`id_stanowisko`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE produkt ADD CONSTRAINT FOREIGN KEY (`id_zdjecia`) REFERENCES `zdjecia`(`id_zdjecia`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE zamowienie ADD CONSTRAINT FOREIGN KEY (`id_klient`) REFERENCES `klient`(`id_klient`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE zamowienie ADD CONSTRAINT FOREIGN KEY (`id_produkt`) REFERENCES `produkt`(`id_produkt`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE zamowienie ADD CONSTRAINT FOREIGN KEY (`id_pracownik`) REFERENCES `pracownik`(`id_pracownik`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE uzytkownik ADD CONSTRAINT FOREIGN KEY (`id_klient`) REFERENCES `klient`(`id_klient`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE uzytkownik ADD CONSTRAINT FOREIGN KEY (`id_koszyk`) REFERENCES `koszyk`(`id_koszyk`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE koszyk ADD CONSTRAINT FOREIGN KEY (`id_zamowienie`) REFERENCES `zamowienie`(`id_zamowienie`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE koszyk ADD CONSTRAINT FOREIGN KEY (`id_zamowienie`) REFERENCES `zamowienie`(`id_zamowienie`) ON DELETE CASCADE ON UPDATE CASCADE; 
